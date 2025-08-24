b744378f2a1e48d1888a04a1387b51f585d8e945
Ankita Chougule <ankitachougule2799@gmail.com>
2025-08-24 22:06:13 +0530

    Create day10.js
    day10 challenge

---
+++ day10.js
@@ -0,0 +1,25 @@
+function groupAnagrams(strs) {
+    let map = {};
+
+    for (let str of strs) {
+        let key = str.split("").sort().join("");
+
+        if (!map[key]) {
+            map[key] = [];
+        }
+
+        map[key].push(str);
+    }
+
+    return Object.values(map);
+}
+
+console.log(groupAnagrams(["eat", "tea", "tan", "ate", "nat", "bat"]));
+
+console.log(groupAnagrams([""]));
+
+console.log(groupAnagrams(["a"]));
+
+console.log(groupAnagrams(["abc", "bca", "cab", "xyz", "zyx", "yxz"]));
+
+console.log(groupAnagrams(["abc", "def", "ghi"]));
